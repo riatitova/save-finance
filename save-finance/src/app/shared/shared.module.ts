783920f376b86d29267fc2b6661044e130621d52
Maria Titova <mary.titova.1995@gmail.com>
2021-04-17 18:39:46 +0400

    feat: add header

--- save-finance/src/app/shared/shared.module.ts
+++ save-finance/src/app/shared/shared.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './components/button/button.component';
-import {ContentBorderComponent} from './components/content-border/content-border.component';
+import { ContentBorderComponent } from './components/content-border/content-border.component';
 import { LogoComponent } from './components/logo/logo.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [ButtonComponent, ContentBorderComponent, LogoComponent],
-  imports: [
-    CommonModule
+  declarations: [
+    ButtonComponent,
+    ContentBorderComponent,
+    LogoComponent,
+    HeaderComponent,
   ],
+  imports: [CommonModule],
   exports: [
     ButtonComponent,
     ContentBorderComponent,
-    LogoComponent
-  ]
+    LogoComponent,
+    HeaderComponent,
+  ],
 })
-export class SharedModule { }
+export class SharedModule {}
