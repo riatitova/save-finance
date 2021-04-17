81a4a421e8f645fe0973b049170e98e15d20c5ee
Maria Titova <mary.titova.1995@gmail.com>
2021-04-17 17:15:05 +0400

    feat: add button

---
+++ save-finance/src/app/shared/components/button/button.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.scss']
+})
+export class ButtonComponent {}
