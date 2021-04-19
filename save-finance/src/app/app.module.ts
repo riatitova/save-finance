7f1893844da332b0ad6ed3baa051df5bb8e318f7
Maria Titova <mary.titova.1995@gmail.com>
2021-04-19 17:05:14 +0400

    fix: shared

--- save-finance/src/app/app.module.ts
+++ save-finance/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
--- save-finance/src/app/shared/shared.module.ts
+++ save-finance/src/app/shared/shared.module.ts
@@ -3,24 +3,18 @@ import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './components/button/button.component';
 import { ContentBorderComponent } from './components/content-border/content-border.component';
 import { LogoComponent } from './components/logo/logo.component';
-import { HeaderComponent } from './components/header/header.component';
-import { NavigationComponent } from './components/navigation/navigation.component';
 
 @NgModule({
   declarations: [
     ButtonComponent,
     ContentBorderComponent,
     LogoComponent,
-    HeaderComponent,
-    NavigationComponent,
   ],
   imports: [CommonModule],
   exports: [
     ButtonComponent,
     ContentBorderComponent,
     LogoComponent,
-    HeaderComponent,
-    NavigationComponent,
   ],
 })
 export class SharedModule {}
