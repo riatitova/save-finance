58f88b036a6806a3d156a90483fcb36a184014c9
Maria Titova <mary.titova.1995@gmail.com>
2021-05-09 21:01:16 +0400

    feat: add articles

--- save-finance/src/app/components/core.module.ts
+++ save-finance/src/app/components/core.module.ts
@@ -12,6 +12,7 @@ import { WeatherComponent } from './weather/weather.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { RegistrationButtonComponent } from './registration-button/registration-button.component';
 import { LogoutButtonComponent } from './logout-button/logout-button.component';
+import { ArticlesComponent } from './articles/articles.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { LogoutButtonComponent } from './logout-button/logout-button.component';
     WeatherComponent,
     RegistrationComponent,
     RegistrationButtonComponent,
-    LogoutButtonComponent
+    LogoutButtonComponent,
+    ArticlesComponent
   ],
   imports: [CommonModule, FormsModule, RouterModule, SharedModule, ReactiveFormsModule],
   exports: [
@@ -35,7 +37,8 @@ import { LogoutButtonComponent } from './logout-button/logout-button.component';
     WeatherComponent,
     RegistrationComponent,
     RegistrationButtonComponent,
-    LogoutButtonComponent
+    LogoutButtonComponent,
+    ArticlesComponent
   ],
 })
 export class CoreModule {}
---
+++ save-finance/src/types/ArticlesResponse.ts
@@ -0,0 +1,302 @@
+const data = {
+  message: '',
+  result: [
+    {
+      id: 243901,
+      url:
+        'https://vc.ru/finance/243901-pyat-klyuchevyh-vyvodov-iz-knigi-manifest-investora',
+      author: {
+        id: 584678,
+        url: 'https://vc.ru/u/584678-kabinet-investora',
+        name: 'Кабинет инвестора',
+        type: 1,
+        avatar: {
+          type: 'image',
+          data: {
+            uuid: 'dcf2ac63-28ad-a986-4f66-0e71a6bee4a2',
+            width: 640,
+            height: 640,
+            size: 12880,
+            type: 'jpg',
+            color: '2c2c2c',
+            hash: '606870600000',
+            external_service: [],
+          },
+        },
+        avatar_url:
+          'https://leonardo.osnova.io/dcf2ac63-28ad-a986-4f66-0e71a6bee4a2/',
+        is_online: false,
+        is_verified: false,
+        is_subscribed: false,
+      },
+      badges: [
+        {
+          type: 'top',
+          text: 'Финансы',
+          background: '#ffffff',
+          color: '#717173',
+          border: '#ffffff',
+        },
+      ],
+      commentsCount: 4,
+      commentsSeenCount: null,
+      favoritesCount: 13,
+      cover: {
+        additionalData: {
+          size: 199238,
+          type: 'jpg',
+          uuid: 'ec9f9add-3461-5862-9b1b-6da4ec234dde',
+        },
+        size: {
+          width: 1920,
+          height: 1080,
+        },
+        thumbnailUrl:
+          'https://leonardo.osnova.io/ec9f9add-3461-5862-9b1b-6da4ec234dde/-/scale_crop/800/-/progressive/yes/',
+        type: 1,
+        url: 'https://leonardo.osnova.io/ec9f9add-3461-5862-9b1b-6da4ec234dde/',
+        size_simple: 'big',
+      },
+      date: 1620557229,
+      dateRFC: 'Sun, 09 May 2021 13:47:09 +0300',
+      date_favorite: null,
+      last_modification_date: 1620557230,
+      hitsCount: 130,
+      intro:
+        'Манифест инвестора — это одна из лучших книг по инвестированию, написанная Уильямом Бернстайном. В своей книге он четко и доступно объясняет теорию и психологию бизнеса в мире инвестирования.',
+      introInFeed: null,
+      isEnabledComments: true,
+      isEnabledLikes: true,
+      isFavorited: false,
+      isRepost: false,
+      likes: {
+        is_liked: 0,
+        count: 1,
+        summ: 1,
+        is_hidden: false,
+      },
+      subsite: {
+        id: 199119,
+        url: 'https://vc.ru/finance',
+        type: 2,
+        name: 'Финансы',
+        description: 'Корпоративные финансы, банковский бизнес, финтех.',
+        avatar: {
+          type: 'image',
+          data: {
+            uuid: '75ebae9d-3e59-f65a-d7b3-145edeb75f90',
+            width: 0,
+            height: 0,
+            size: 1,
+            type: 'jpg',
+            color: '',
+            hash: '',
+            external_service: [],
+          },
+        },
+        avatar_url:
+          'https://leonardo.osnova.io/75ebae9d-3e59-f65a-d7b3-145edeb75f90/',
+        head_cover: null,
+        is_verified: false,
+        is_enable_writing: false,
+        is_subscribed: false,
+        is_subscribed_to_new_posts: false,
+        prev_entry: {
+          id: 243193,
+          title:
+            '«Совкомбанк» рассказал о «преступной» схеме: клиенты переводили деньги между счетами в разных банках ради бонусных миль',
+          description:
+            'Банк заблокировал клиентов, чьи операции были не похожи на обычную практику использования карт.',
+        },
+      },
+      similar: [],
+      title: 'Пять ключевых выводов из книги: Манифест инвестора',
+      type: 1,
+      entryContent: {
+        html: ' <div></div>',
+        version: '2179010269.99',
+      },
+      commentatorsAvatars: [
+        'https://leonardo.osnova.io/f0dd818d-4f28-59f4-b75c-7f597feba1f4/',
+        'https://leonardo.osnova.io/ed3da812-1bc8-5565-93b6-7314791318a4/',
+        'https://leonardo.osnova.io/e6b2961f-cc80-497c-388b-26baae1ad98d/',
+        'https://png.cmtt.space/user-userpic/80/24/65/51ceaba2729265.jpg',
+        'https://leonardo.osnova.io/64da0741-13ea-8c40-cce1-a7b95c4a605a/',
+        'https://leonardo.osnova.io/4cd744e8-a4ec-b96e-cf91-7e6321345bb3/',
+        'https://leonardo.osnova.io/37443886-2041-5bf4-89ee-e5200805f79d/',
+        'https://leonardo.osnova.io/6a1a73dc-1611-def1-68d5-de55a8e71117/',
+        'https://leonardo.osnova.io/37850c89-cdef-7a26-07dd-2fedbd130b1c/',
+        'https://leonardo.osnova.io/8d98e351-27bd-5c66-a7c5-9bb4690a53d2/',
+        'https://leonardo.osnova.io/a7902340-49cf-5d72-bea7-02ee7ac9185b/',
+      ],
+      webviewUrl: null,
+      isPinned: false,
+      canEdit: false,
+      etcControls: {},
+      highlight: '',
+      blocks: [
+        {
+          type: 'media',
+          data: {
+            items: [
+              {
+                title: '',
+                author: '',
+                image: {
+                  type: 'image',
+                  data: {
+                    uuid: 'ec9f9add-3461-5862-9b1b-6da4ec234dde',
+                    width: 1920,
+                    height: 1080,
+                    size: 199238,
+                    type: 'jpg',
+                    color: 'd8d8db',
+                    hash: '',
+                    external_service: [],
+                  },
+                },
+              },
+            ],
+            with_background: false,
+            with_border: false,
+          },
+          cover: true,
+          anchor: '',
+        },
+        {
+          type: 'text',
+          data: {
+            text:
+              'Манифест инвестора — это **одна из лучших** книг по инвестированию, написанная Уильямом Бернстайном. В своей книге он четко и доступно объясняет **теорию и психологию бизнеса в мире инвестирования**.',
+            text_truncated: '<<<same>>>',
+          },
+          cover: true,
+          anchor: '',
+        },
+        {
+          type: 'text',
+          data: {
+            text:
+              'Бернстайн начинает с обзора финансовой теории, сопровождаемой **реальными историческими примерами**. Далее знакомит читателей с поведенческими ловушками, на которые они могут натолкнуться, и завершает механикой построения долгосрочного портфеля.',
+            text_truncated: '<<<same>>>',
+          },
+          cover: false,
+          anchor: '',
+        },
+        {
+          type: 'text',
+          data: {
+            text:
+              'Инвестиционная мудрость начинается с осознания того, что **только долгосрочная прибыль имеет значение**. При построении портфеля Бернстайн советует выбирать активы с отрицательной корреляцией друг к другу — это снижает уровень риска и повышает итоговую доходность из-за ребалансировки портфеля.',
+            text_truncated: '<<<same>>>',
+          },
+          cover: false,
+          anchor: '',
+        },
+        {
+          type: 'text',
+          data: {
+            text:
+              'Автор объясняет, почему **невозможно получить высокую доходность при низком уровне риска**. Если инвестор стремится к абсолютной безопасности — он **обречен на низкую доходность**. В то же время, обещание высокой доходности при низком риске — это самый надежный индикатор **мошенничества**.',
+            text_truncated: '<<<same>>>',
+          },
+          cover: false,
+          anchor: '',
+        },
+        {
+          type: 'text',
+          data: {
+            text:
+              'Будучи неврологом по образованию, Уильям Бернстайн утверждает, что самое главное качество инвестора — это **эмоциональная дисциплина.**',
+            text_truncated: '<<<same>>>',
+          },
+          cover: false,
+          anchor: '',
+        },
+        {
+          type: 'text',
+          data: {
+            text: 'Бернштейн.',
+            text_truncated: '<p>Бернштейн ',
+          },
+          cover: false,
+          anchor: '',
+        },
+        {
+          type: 'text',
+          data: {
+            text:
+              '**Манифест инвестора единым сюжетом объединяет пять ключевых моментов:**',
+            text_truncated: '<<<same>>>',
+          },
+          cover: false,
+          anchor: '',
+        },
+        {
+          type: 'text',
+          data: {
+            text:
+              '• Ничто не сделает вас беднее, чем ваши эмоции. Также, инвестирование не должно быть “веселым” или “приятным”. Потратьте деньги на прыжки с парашютом, если хотите веселья и ураган эмоций. • Не покупайте отдельные акции и распределяйте активы с помощью индексного инвестирования. • Держитесь подальше от активно управляемых фондов. • Помните о взаимосвязи риска с доходностью. • Инвестируйте всегда, особенно в плохие времена.',
+            text_truncated:
+              '<p>• Ничто не сделает вас беднее, чем ваши эмоции. Также, инвестирование не должно быть “веселым” или “приятным”. Потратьте деньги на прыжки с парашютом, если хотите веселья и ураган эмоций.<br />• Не покупайте отдельные акции и распределяйте активы с помощью индексного инвестирования.<br />• Держитесь подальше от активно управляемых фондов.<br />• Помните о…</p>',
+          },
+          cover: false,
+          anchor: '',
+        },
+        {
+          type: 'text',
+          data: {
+            text:
+              'Манифест инвестора — хорошее описание базовой инвестиционной стратегии, написанное в слегка юмористическом стиле. Для начинающих инвесторов — книга **поможет избежать наиболее распространённые ошибки**. А для опытным — послужит отличным **напоминанием о важных аспектах инвестирования**.',
+            text_truncated: '<<<same>>>',
+          },
+          cover: false,
+          anchor: '',
+        },
+        {
+          type: 'text',
+          data: {
+            text:
+              'Скачать и прочитать книгу можно по [ссылке](https://t.me/investlib/687) из Библиотеки инвестора.',
+            text_truncated: '<<<same>>>',
+          },
+          cover: false,
+          anchor: '',
+        },
+        {
+          type: 'text',
+          data: {
+            text: '[#книги](?q=%23%D0%BA%D0%BD%D0%B8%D0%B3%D0%B8)',
+            text_truncated: '<<<same>>>',
+          },
+          cover: false,
+          anchor: '',
+        },
+        {
+          type: 'text',
+          data: {
+            text:
+              'Подписывайтесь на [Кабинет инвестора](https://t.me/investingcorp) в Telegram! Там еще много интересного!',
+            text_truncated: '<<<same>>>',
+          },
+          cover: false,
+          anchor: '',
+        },
+      ],
+      subscribedToTreads: false,
+      is_show_thanks: false,
+      is_still_updating: false,
+      is_filled_by_editors: false,
+      isEditorial: false,
+      audioUrl: null,
+      hotness: 0,
+      commentEditor: {
+        enabled: true,
+      },
+      summarize:
+        'Манифест инвестора — это одна из лучших книг по инвестированию, написанная Уильямом Бернстайном. В своей книге он четко и доступно объясняет теорию и психологию бизнеса в мире инвестирования. Бернстайн начинает с обзора финансовой теории, сопровождаемой реальными историческими примерами.',
+    },
+  ],
+};
+
+export type ArticlesResponse = typeof data;
